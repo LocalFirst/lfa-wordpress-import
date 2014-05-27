3d5908bef998aef2d704e77ee203f49b84624b12
Curtis Miller <curtis@velocitylabs.io>
2014-05-27 09:55:13 -0700

    Format URLs in post content

--- lib/wordpress/page.rb
+++ lib/wordpress/page.rb
@@ -10,7 +10,7 @@ module WordPressImport
     end
 
     def inspect
-      "WordPress::Page(#{post_id}): #{title}"     
+      "WordPress::Page(#{post_id}): #{title}"
     end
 
     def link
@@ -26,14 +26,14 @@ module WordPressImport
     end
 
     def content_formatted
-      formatted = format_shortcodes(format_syntax_highlighter(format_paragraphs(content)))
+      formatted = format_shortcodes(format_syntax_highlighter(format_paragraphs(format_urls(content))))
 
       # remove all tags inside <pre> that simple_format created
       # TODO: replace format_paragraphs with a method, that ignores pre-tags
-      formatted.gsub!(/(<pre.*?>)(.+?)(<\/pre>)/m) do |match| 
+      formatted.gsub!(/(<pre.*?>)(.+?)(<\/pre>)/m) do |match|
         "#{$1}#{strip_tags($2)}#{$3}"
       end
-        
+
       formatted
     end
 
@@ -89,20 +89,20 @@ module WordPressImport
 
     def to_rails
       # :user_id => creator
-      page = ::Page.create!(:id => post_id, :title => title, 
-        :created_at => post_date, :slug => post_name, 
+      page = ::Page.create!(:id => post_id, :title => title,
+        :created_at => post_date, :slug => post_name,
         :published_at => publish_date, :body => content_formatted)
     end
 
-    private 
+    private
 
     def format_paragraphs(text, html_options={})
       # WordPress doesn't export <p>-Tags, so let's run a simple_format over
-      # the content. As we trust ourselves, no sanatize. This code is heavily
+      # the content. As we trust ourselves, no sanitize. This code is heavily
       # inspired by the simple_format rails helper
       text = ''.html_safe if text.nil?
       start_tag = tag('p', html_options, true)
-      
+
       text.gsub!(/\n\n+/, "</p>#{start_tag}")  # 2+ newline  -> paragraph
       text.gsub!(/\r?\n/, "<br/>\n")               # \r\n and \n -> line break (must be after the paragraph detection to avoid <br/><br/>)
       text.insert 0, start_tag
@@ -115,10 +115,10 @@ module WordPressImport
       # In WordPress you can (via a plugin) enclose code in [lang][/lang]
       # blocks, which are converted to a <pre>-tag with a class corresponding
       # to the language.
-      # 
+      #
       # Example:
-      # [ruby]p "Hello World"[/ruby] 
-      # -> <pre class="brush: ruby">p "Hello world"</pre> 
+      # [ruby]p "Hello World"[/ruby]
+      # -> <pre class="brush: ruby">p "Hello world"</pre>
       text.gsub(/\[(\w+)\](.+?)\[\/\1\]/m, '<pre class="brush: \1">\2</pre>')
     end
 
@@ -139,6 +139,10 @@ module WordPressImport
       end
 
       Shortcode.process(text)
+    end
+
+    def format_urls text
+      text.gsub(/(href|src)\=(\"|\')(http:\/\/localfirstaz\.com)/i, '\1=\2')
     end
   end
 end
